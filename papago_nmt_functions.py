046a8fa830cc5f7734039e3aec69fda64cef5a80
changdol <edueman@naver.com>
2019-09-16 18:18:38 +0900

    Papago Nmt Cloud Functions Action 소스 추가

---
+++ papago_nmt_functions.py
@@ -0,0 +1,47 @@
+import requests
+
+
+def main(args):
+
+    text = args.get("translate_text", "안녕하세요")
+
+    client_id = ""
+    client_secret = ""
+
+    url = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation"
+
+    data = "source=ko&target=en&text=" + ''
+
+    #text = "네이버 클라우드 플랫폼에서는 Clova, papago 등 네이버의 다양한 인공지능 서비스를 API 형태로 제공합니다. 고객은 네이버의 인공지능 서비스를 활용하여, " \
+    #       "최신 기술 기반의 AI 서비스를 구축하고 안정적으로 운영할 수 있습니다.."
+
+    # ko : 한국어
+    # en : 영어
+    # zh-CN : 중국어 간체
+    # zh-TW : 중국어 번체
+    # es: 스페인어
+    # fr: 프랑스어
+    # vi: 베트남어
+    # th: 태국어
+    # id: 인도네시아어
+
+
+    val = {
+        "source": 'ko',
+        "target": 'zh-CN',
+        "text": text
+    }
+
+    headers = {
+        "X-NCP-APIGW-API-KEY-ID": client_id,
+        "X-NCP-APIGW-API-KEY": client_secret
+    }
+
+    response = requests.post(url,  data=val, headers=headers)
+    rescode = response.status_code
+
+    if rescode == 200:
+        return {"result": response.text}
+        print(response.text)
+    else:
+        return {"result": response.text}

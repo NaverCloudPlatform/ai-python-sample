5f6a119bd4e3e5cbaf64f536e81ad1002b4c9332
changdol <edueman@naver.com>
2020-05-07 14:12:25 +0900

    OCR general API

--- ocr_general_api.py
+++ ocr_general_api.py
@@ -13,44 +13,32 @@ class OCRTestSample:
     ep_path = ''
     # ocr secret key
     secret_key = ''
-    # IAM AccessKey
-    access_key = ''
 
     def req_message_send(self):
 
         timestamp = self.get_timestamp()
 
-        data =self.fileToBase64('./contents/ocr_sample_text.jpg')
+        data =self.fileToBase64('./contents/등기부등본.png')
 
         request_body = {
-
             'version': 'V1',
             'requestId': 'test_ocr_edueman',
             'timestamp': str(timestamp),
             'lang': 'ko',
-            'images': [{ 'format': 'jpg', 'data': data,
-                         'name':'korea_history'}]
-            # 'images': [{ 'format': 'jpg', 'url': 'https://kr.object.ncloudstorage.com/labs/ocr/test_sample_03.jpg',
-            #              'name':'korea_history'}]
+            'images': [{ 'format': 'jpg', 'data': data, 'name':'korea_history'}]
         }
 
         ## Request body
         encode_request_body = json.dumps(request_body).encode('UTF-8')
 
-        ## make signature
-        signature = self.make_signature(self.secret_key, encode_request_body)
-
         ## headers
         custom_headers = {
             'Content-Type': 'application/json;UTF-8',
-            'x-ncp-apigw-timestamp': str(timestamp),
-            'x-ncp-iam-access-key': self.access_key,
             'X-OCR-SECRET': self.secret_key
         }
 
         print("## Encode Request Body : ", encode_request_body)
         print("## Timestamp : ", timestamp)
-        print("## Signature : ", signature)
         print("## headers ", custom_headers)
         print("## request body : ", json.dumps(request_body))
 
@@ -71,20 +59,12 @@ class OCRTestSample:
         timestamp = int(time.time() * 1000)
         return timestamp
 
-    @staticmethod
-    def make_signature(access_key, request_body):
-
-        secret_key_bytes = bytes(access_key, 'UTF-8')
-
-        signing_key = base64.b64encode(hmac.new(secret_key_bytes, request_body, digestmod=hashlib.sha256).digest())
-
-        return signing_key
-
 
 if __name__ == '__main__':
 
     start = time.time()
     res = OCRTestSample().req_message_send()
+    print(res.status_code)
 
     if(res.status_code == 200):
         data = json.loads(res.text)
